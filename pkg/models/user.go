4697c4e7822d9a9ca1f251be19f216fed713533b
Martin <martinjosh@vashcorp.com>
2024-05-19 00:18:13 +0800

    Add user password field

--- pkg/models/user.go
+++ pkg/models/user.go
@@ -7,4 +7,5 @@ type User struct {
 	FirstName string `json:"first_name" binding:"required"`
 	LastName  string `json:"last_name" binding:"required"`
 	Email     string `json:"email" binding:"required" gorm:"unique"`
+	Password  string `json:"password"`
 }
