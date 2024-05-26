5b79e46a2c4979be4f23417aac0337ed3d530a04
jkain88 <torricermartinjosh@gmail.com>
2024-05-26 22:31:54 +0800

    Add populate_db script for transactions

---
+++ scripts/populate_db.go
@@ -0,0 +1,83 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/jkain88/finance-tracking/pkg/db"
+	"github.com/jkain88/finance-tracking/pkg/models"
+	"gorm.io/gorm"
+)
+
+func populateTransactions(db *gorm.DB) {
+	//Define a list of transactions
+	transactions := []models.Transaction{
+		{
+			AccountID:  1,
+			CategoryID: 1,
+			UserID:     1,
+			Name:       "Test Transaction 1",
+			Type:       "debit",
+			Amount:     100,
+		},
+		{
+			AccountID:  2,
+			CategoryID: 2,
+			UserID:     1,
+			Name:       "Test Transaction 2",
+			Type:       "credit",
+			Amount:     100,
+		},
+		{
+			AccountID:  2,
+			CategoryID: 2,
+			UserID:     1,
+			Name:       "Test Transaction 2",
+			Type:       "credit",
+			Amount:     100,
+		},
+		{
+			AccountID:  2,
+			CategoryID: 2,
+			UserID:     1,
+			Name:       "Test Transaction 2",
+			Type:       "credit",
+			Amount:     100,
+		},
+		{
+			AccountID:  2,
+			CategoryID: 2,
+			UserID:     1,
+			Name:       "Test Transaction 2",
+			Type:       "credit",
+			Amount:     100,
+		},
+		{
+			AccountID:  2,
+			CategoryID: 2,
+			UserID:     1,
+			Name:       "Test Transaction 2",
+			Type:       "credit",
+			Amount:     100,
+		},
+	}
+
+	//Populate the transactions in the database
+	for _, transaction := range transactions {
+		fmt.Println(transaction)
+		result := db.Create(&transaction)
+		if result.Error != nil {
+			panic(result.Error.Error())
+		}
+		transaction.CreatedAt = time.Now().AddDate(0, 0, rand.Intn(3))
+		db.Save(&transaction)
+	}
+}
+
+func main() {
+	db := db.Init()
+
+	// Populate transactions
+	populateTransactions(db)
+}
